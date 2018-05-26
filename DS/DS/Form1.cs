bab223402a4bbd789909eecb33a7e06678e35df8
Ich-wei <whita@outlook.jp>
2018-05-26 16:45:40 +0900

    データセットでのDB操作アプリ作成

---
+++ DS/DS/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database1DataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: このコード行はデータを 'database1DataSet.Table' テーブルに読み込みます。必要に応じて移動、または削除をしてください。
+            this.tableTableAdapter.Fill(this.database1DataSet.Table);
+
+        }
+    }
+}
